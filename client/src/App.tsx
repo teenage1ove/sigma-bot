b0c8b8d6d04340aed7b46288ef8360b2fde9a51e
teenage1ove <kirillstars7@gmail.com>
2024-12-07 21:29:21 +0300

    init server and client

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,34 +1,40 @@
+import {init, viewport, themeParams, mainButton} from '@telegram-apps/sdk'
 import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+
+init()
+
+viewport.mount()
+themeParams.mount()
+mainButton.mount()
+
+viewport.expand()
+themeParams.bindCssVars()
+mainButton.setParams({
+  hasShineEffect: true,
+  isEnabled: true,
+  isVisible: true,
+  text: 'Hello'
+})
+
+
 
 function App() {
-  const [count, setCount] = useState(0)
+
+  const [message, setMessage] = useState('')
+
+  mainButton.onClick(() => {
+    mainButton.setParams({
+      isVisible: false
+    })
+
+    setMessage('Button clicked!')
+  })
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className='flex flex-col justify-center items-center h-screen bg-black'>
+      <h1 className='text-4xl text-white'>Hello</h1>
+      {message && <p className='text-2xl text-white'>{message}</p>}
+    </div>
   )
 }
 
